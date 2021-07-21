74cd75e6366b5da4de0833fcacbc0fed0f3446df
VanLinden <nshtromberger@gmail.com>
2021-07-21 15:35:58 +0400

    Update TodoTask.tsx

--- src/Components/TodoTask.tsx
+++ src/Components/TodoTask.tsx
@@ -4,17 +4,25 @@ import { ITodo } from "../interfaces";
 interface Props {
     todo: ITodo;
     completeTask(todoNameToDelete: string): void; 
+    markAsDone(todoNameToMark: string): void;
 }
 
-const TodoTask = ( {todo, completeTask }: Props) => {
+const TodoTask = ( {todo, markAsDone, completeTask }: Props) => {
     return (
         <div className="todo">
             <div className="content">
-                <span>{ todo.todoName }</span>
+            {todo.isDone.toString() === "false" && <span>{ todo.title }</span>}
+            {todo.isDone.toString() === "true" && <span className="contred">{ todo.title }</span>}
             </div>
-            <button 
-            onClick={() => {
-                completeTask(todo.todoName);}}>X</button>
+            {todo.isDone.toString() === "false" && <button className="mark" onClick={() => { markAsDone(todo.title); }}>
+                Mark as done
+            </button>}
+            {todo.isDone.toString() === "true" && <button className="mark" onClick={() => { markAsDone(todo.title); }}>
+                Mark as not done
+            </button>}
+            <button onClick={() => {completeTask(todo.title);}}>
+                    X
+            </button>
         </div>
     )
 }
